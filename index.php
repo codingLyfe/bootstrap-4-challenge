6b628eb984eaf3a780137a8f8e04de642ab8069d
Tristan Bennett <tmbennett2011@msn.com>
2018-02-04 15:46:46 -0700

    started challenge, added basic structure

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Bootstrap 4 Challenge</title>
+	</head>
+	<body>
+		<header class = "container">
+			<h1>Hello, Bootstrap!</h1>
+			<p>Today I will be trying to figure out how to make an html page look like the example provided by Rochelle Lewis by integrating bootstrap 4. ("rm -rf").</p>
+			<button type = "button">Learn More</button>
+		</header>
+		<main>
+			<aside class = "container">
+				<h2>Bootstrap 4 Challenge</h2>
+				<ul>
+					<li>[link 1]</li>
+					<li>[link 2]</li>
+					<li>[link 3]</li>
+					<li>[link 4]</li>
+				</ul>
+			</aside>
+
+			<div class = "container">
+				<img src="" alt="">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor elit sed vulputate mi sit amet mauris commodo quis. Malesuada fames ac turpis egestas sed. Aliquam id diam maecenas ultricies mi eget. Faucibus vitae aliquet nec ullamcorper sit amet risus. Lacus luctus accumsan tortor posuere ac. Odio morbi quis commodo odio aenean sed adipiscing diam donec. Mattis aliquam faucibus purus in massa. Sed euismod nisi porta lorem mollis. Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Amet massa vitae tortor condimentum lacinia. Eget lorem dolor sed viverra ipsum nunc aliquet bibendum enim. Sagittis aliquam malesuada bibendum arcu vitae elementum curabitur. Turpis cursus in hac habitasse platea dictumst quisque. Montes nascetur ridiculus mus mauris vitae ultricies leo. Orci porta non pulvinar neque laoreet. Duis at consectetur lorem donec massa. Adipiscing vitae proin sagittis nisl rhoncus mattis. Quam nulla porttitor massa id neque aliquam vestibulum. Senectus et netus et malesuada fames ac.</p>
+			</div>
+
+			<div class = "container">
+				<div class = "row">
+					<div class = "column">
+						<img src="" alt="">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor elit sed vulputate mi sit amet mauris commodo quis. Malesuada fames ac turpis egestas sed. Aliquam id diam maecenas ultricies mi eget</p>
+					</div>
+				</div>
+				<div class = "row">
+					<div class = "column">
+						<img src="" alt="">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor elit sed vulputate mi sit amet mauris commodo quis. Malesuada fames ac turpis egestas sed. Aliquam id diam maecenas ultricies mi eget</p>
+					</div>
+				</div>
+				<div class = "row">
+					<div class = "column">
+						<img src="" alt="">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor elit sed vulputate mi sit amet mauris commodo quis. Malesuada fames ac turpis egestas sed. Aliquam id diam maecenas ultricies mi eget</p>
+					</div>
+				</div>
+				<div class = "row">
+					<div class = "column">
+						<img src="" alt="">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor elit sed vulputate mi sit amet mauris commodo quis. Malesuada fames ac turpis egestas sed. Aliquam id diam maecenas ultricies mi eget</p>
+					</div>
+				</div>
+			</div>
+		</main>
+	</body>
+</html>
