b7c36cadb4f65d9788b8c0e0251ec42fd4e2d70b
Tristan Bennett <tmbennett2011@msn.com>
2018-02-04 20:22:58 -0700

    done

--- index.php
+++ index.php
@@ -26,10 +26,11 @@
 
 
 	<body>
+
 		<div class="container">
-			<nav class="navbar navbar-expand-lg navbar-toggleable-md navbar-light bg-light border rounded my-3">
+			<nav class="navbar navbar-expand-lg navbar-toggleable-md bg-light navbar-light border rounded my-3">
 				<h1 class="navbar-brand">Bootstrap</h1>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"  aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 				<div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
