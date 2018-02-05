a4c8706d8a7ba087486582b36feb1cd47cc2f308
Tristan Bennett <tmbennett2011@msn.com>
2018-02-04 19:27:20 -0700

    struggling with image moving to left of text

--- index.php
+++ index.php
@@ -3,55 +3,119 @@
 	<head>
 		<meta charset="UTF-8"/>
 		<title>Bootstrap 4 Challenge</title>
+
+		<!-- sets default bootstrap viewport -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+				integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+		<!-- Personal CSS -->
+		<link rel="stylesheet" href="css/style.css">
+
+		<!-- jQuery first, then Popper.js, then bootstrap JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+				  integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+				  crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+				  integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+				  crossorigin="anonymous"></script>
 	</head>
+
+
 	<body>
-		<header class = "container">
+
+		<aside class="container">
+			<h2>Bootstrap 4 Challenge</h2>
+			<ul>
+				<li>[link 1]</li>
+				<li>[link 2]</li>
+				<li>[link 3]</li>
+				<li>[link 4]</li>
+			</ul>
+		</aside>
+
+
+		<div class="jumbotron my-5 mx-5 rounded">
 			<h1>Hello, Bootstrap!</h1>
-			<p>Today I will be trying to figure out how to make an html page look like the example provided by Rochelle Lewis by integrating bootstrap 4. ("rm -rf").</p>
-			<button type = "button">Learn More</button>
-		</header>
-		<main>
-			<aside class = "container">
-				<h2>Bootstrap 4 Challenge</h2>
-				<ul>
-					<li>[link 1]</li>
-					<li>[link 2]</li>
-					<li>[link 3]</li>
-					<li>[link 4]</li>
-				</ul>
-			</aside>
+			<p>Today I will be trying to figure out how to make an html page look like the example provided by Rochelle
+				Lewis by integrating bootstrap 4. ("rm -rf").</p>
+			<p class="lead">
+				<a class="btn btn-primary" href="#" role="button">Learn more</a>
+			</p>
+		</div>
 
-			<div class = "container">
-				<img src="" alt="">
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor elit sed vulputate mi sit amet mauris commodo quis. Malesuada fames ac turpis egestas sed. Aliquam id diam maecenas ultricies mi eget. Faucibus vitae aliquet nec ullamcorper sit amet risus. Lacus luctus accumsan tortor posuere ac. Odio morbi quis commodo odio aenean sed adipiscing diam donec. Mattis aliquam faucibus purus in massa. Sed euismod nisi porta lorem mollis. Mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Amet massa vitae tortor condimentum lacinia. Eget lorem dolor sed viverra ipsum nunc aliquet bibendum enim. Sagittis aliquam malesuada bibendum arcu vitae elementum curabitur. Turpis cursus in hac habitasse platea dictumst quisque. Montes nascetur ridiculus mus mauris vitae ultricies leo. Orci porta non pulvinar neque laoreet. Duis at consectetur lorem donec massa. Adipiscing vitae proin sagittis nisl rhoncus mattis. Quam nulla porttitor massa id neque aliquam vestibulum. Senectus et netus et malesuada fames ac.</p>
-			</div>
 
-			<div class = "container">
-				<div class = "row">
-					<div class = "column">
-						<img src="" alt="">
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor elit sed vulputate mi sit amet mauris commodo quis. Malesuada fames ac turpis egestas sed. Aliquam id diam maecenas ultricies mi eget</p>
-					</div>
-				</div>
-				<div class = "row">
-					<div class = "column">
-						<img src="" alt="">
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor elit sed vulputate mi sit amet mauris commodo quis. Malesuada fames ac turpis egestas sed. Aliquam id diam maecenas ultricies mi eget</p>
-					</div>
-				</div>
-				<div class = "row">
-					<div class = "column">
-						<img src="" alt="">
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor elit sed vulputate mi sit amet mauris commodo quis. Malesuada fames ac turpis egestas sed. Aliquam id diam maecenas ultricies mi eget</p>
-					</div>
-				</div>
-				<div class = "row">
-					<div class = "column">
-						<img src="" alt="">
-						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Auctor elit sed vulputate mi sit amet mauris commodo quis. Malesuada fames ac turpis egestas sed. Aliquam id diam maecenas ultricies mi eget</p>
+
+
+			<div class="jumbotron rounded-0">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-4 col-md-12 text-center">
+							<img src="images/bootstrap-img.png" alt="bootstrap image" class="img-fluid">
+						</div>
+						<div clas="col-lg-8 col-md-12">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+							et	dolore magna aliqua. Auctor elit sed vulputate mi sit amet mauris commodo quis. Malesuada fames ac
+							turpis egestas sed. Aliquam id diam maecenas ultricies mi eget. Faucibus vitae aliquet nec ullamcorper sit
+							amet risus. Lacus luctus accumsan tortor posuere ac. Odio morbi quis commodo odio aenean sed adipiscing
+							diam donec. Mattis aliquam faucibus purus in massa. Sed euismod nisi porta lorem mollis. Mauris
+							pellentesque pulvinar pellentesque habitant morbi tristique senectus. Amet massa vitae tortor condimentum
+							lacinia. Eget lorem dolor sed viverra ipsum nunc aliquet bibendum enim. Sagittis aliquam malesuada bibendum
+							arcu vitae elementum curabitur. Turpis cursus in hac habitasse platea dictumst quisque. Montes nascetur
+							ridiculus mus mauris vitae ultricies leo. Orci porta non pulvinar neque laoreet. Duis at
+							consectetur lorem donec massa. Adipiscing vitae proin sagittis nisl rhoncus mattis. Quam nulla porttitor massa
+							id neque aliquam vestibulum. Senectus et netus et malesuada fames ac.</p>
+						</div>
 					</div>
 				</div>
 			</div>
-		</main>
+
+
+			<div class="container py-5" px-auto>
+				<div class="row">
+					<div class="card border-0 col-lg-3 col-md-6 col-xs-6 text-center">
+						<img class="card-img-top rounded-circle m-2" src="images/color-blast-ione-hedges.jpg" alt="color blast image">
+						<div class="card-block bg-light border rounded m-2 p-2">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+							labore et dolore magna aliqua. Auctor elit sed vulputate mi sit amet mauris commodo quis. Malesuada
+							fames ac turpis egestas sed. Aliquam id diam maecenas ultricies mi eget</p>
+						</div>
+					</div>
+
+					<div class="card border-0 col-lg-3 col-md-6 col-xs-6 text-center">
+						<img class="card-img-top rounded-circle m-2" src="images/color-blast-ione-hedges.jpg" alt="color blast image">
+						<div class="card-block bg-light border rounded m-2 p-2">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+								labore et dolore magna aliqua. Auctor elit sed vulputate mi sit amet mauris commodo quis. Malesuada
+								fames ac turpis egestas sed. Aliquam id diam maecenas ultricies mi eget</p>
+						</div>
+					</div>
+
+					<div class="card border-0 col-lg-3 col-md-6 col-xs-6 text-center">
+						<img class="card-img-top rounded-circle m-2" src="images/color-blast-ione-hedges.jpg" alt="color blast image">
+						<div class="card-block bg-light border rounded m-2 p-2">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+								labore et dolore magna aliqua. Auctor elit sed vulputate mi sit amet mauris commodo quis. Malesuada
+								fames ac turpis egestas sed. Aliquam id diam maecenas ultricies mi eget</p>
+						</div>
+					</div>
+
+					<div class="card border-0 col-lg-3 col-md-6 col-xs-6 text-center">
+						<img class="card-img-top rounded-circle m-2" src="images/color-blast-ione-hedges.jpg" alt="color blast image">
+						<div class="card-block bg-light border rounded m-2 p-2">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+								labore et dolore magna aliqua. Auctor elit sed vulputate mi sit amet mauris commodo quis. Malesuada
+								fames ac turpis egestas sed. Aliquam id diam maecenas ultricies mi eget</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		<footer class="sfooter" class="sfooter-content">
+			<p>This is a footer! Copyright....IDK!</p>
+		</footer>
 	</body>
 </html>
